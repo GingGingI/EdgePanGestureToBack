bfd8e947d95150b2a74a0f8624f84dc6baa568f6
GingGingI <sma0147@naver.com>
2019-01-28 16:51:44 +0900

    Initial Commit

---
+++ EdgePanGestureToBack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EdgePanGestureToBack
+//
+//  Created by 깅징이 on 28/01/2019.
+//  Copyright © 2019 깅징이. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
