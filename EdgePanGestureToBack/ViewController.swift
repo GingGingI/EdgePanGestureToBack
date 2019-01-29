9a4733b600afe74fbe3410e6a9d53584cf73d69f
GingGingI <sma0147@naver.com>
2019-01-29 18:25:56 +0900

    fade out add
    add function

--- EdgePanGestureToBack/ViewController.swift
+++ EdgePanGestureToBack/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var edgePanGesture: UIScreenEdgePanGestureRecognizer!
 
+//    BackPressdindicatorView
+    @IBOutlet weak var IndicatorView: UIView!
 //    BackPressdConstraint
     @IBOutlet weak var BackIndicator: NSLayoutConstraint!
     
@@ -21,6 +23,7 @@ class ViewController: UIViewController {
     private var MAX: CGFloat = 150.0
 //    Check is BackPressing
     private var onBackProgressing: Bool = false
+    private var AgainBlocker: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,25 +33,51 @@ class ViewController: UIViewController {
     }
     
     @IBAction func edgePanGesture(_ sender: UIScreenEdgePanGestureRecognizer) {
-        if !onBackProgressing {
+        if sender.state == .began { AgainBlocker = false }
+        if sender.state == .ended && !onBackProgressing{
+            onBackProgressing = true
+            
+            UIView.animate(withDuration: 0.3, animations: {
+//                slide out
+//                self.BackIndicator.constant = CGFloat(-150.0)
+                self.IndicatorView.alpha = 0.0
+                
+                self.view.layoutIfNeeded()
+            }, completion: {(finished: Bool) in
+                self.onBackProgressing = false
+                
+//                    use when using Fade out
+                self.BackIndicator.constant = CGFloat(-150.0)
+            })
+        }
+        
+        if !onBackProgressing && !AgainBlocker {
             let Percent: Float = getPercent(max: MAX, value: sender.translation(in: self.view).x)
         
-            if (Percent >= 1.0) {
+            if (Percent >= 1.0 && !onBackProgressing) {
 //                print("DDing!")
+                IndicatorView.alpha = CGFloat(0.5)
+                
                 onBackProgressing = true
                 impact.impactOccurred()
                 
                 UIView.animate(withDuration: 1.2, animations: {
-                    self.BackIndicator.constant = -150
+//                    Slide out
+//                    self.BackIndicator.constant = CGFloat(-150.0)
+//                    Fade out
+                    self.IndicatorView.alpha = 0.0
                     self.view.layoutIfNeeded()
                 }, completion: {(finished: Bool) in
+                    self.AgainBlocker = true
                     self.onBackProgressing = false
+                    
+//                    use when using Fade out
+                    self.BackIndicator.constant = CGFloat(-150.0)
                 })
             } else {
-//                print("panGesture : ", sender.translation(in: self.view).x)
-//                print("percent : ", Percent)
                 UIView.animate(withDuration: 0.05, animations: {
-                    self.BackIndicator.constant = CGFloat(-125.0 + (125 * Percent))
+                    self.IndicatorView.alpha = self.adInterPolator(value: Percent, mag: 0.5)
+                    self.BackIndicator.constant = CGFloat(-150.0 + (150 * Percent))
                     self.view.layoutIfNeeded()
                 })
             }
@@ -59,4 +88,7 @@ class ViewController: UIViewController {
         return Float(value / max)
     }
     
+    func adInterPolator(value: Float, mag: Float) -> CGFloat {
+        return CGFloat(Float(cos(Double.pi * Double(value+1)) / 2 + 0.5) * mag)
+    }
 }
